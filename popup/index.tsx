0380f22ef8d6da0f8c2c832a8787b5a935b00660
ileristovski1 <ileristovski1@gmail.com>
2024-04-11 09:23:24 +0200

    Folder structure

---
+++ popup/index.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react"
+
+import IndexOptions from "../options/index"
+
+const Popup: React.FC = () => {
+  return (
+    <div className="popup">
+      <IndexOptions />
+    </div>
+  )
+}
+
+export default Popup
